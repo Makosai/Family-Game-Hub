061d448469a11d6572702c12bafc5725104b8dd5
Makosai <kristopher.ali.dev@gmail.com>
2023-04-09 04:24:33 -0400

    Upgraded Unity version to 2022.2.11f1.

--- Assets/FGH/Core/Deck.cs
+++ Assets/FGH/Core/Deck.cs
@@ -1,46 +1,53 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Deck : IComparer<Sprite> {
-  public static int Compare(Sprite x, Sprite y) {
-    int xVal = ConvertVal(x.name);
-    int yVal = ConvertVal(y.name);
+public class Deck : IComparer<Sprite>
+{
+    public int Compare(Sprite x, Sprite y)
+    {
+        int xVal = ConvertVal(x.name);
+        int yVal = ConvertVal(y.name);
 
-    if (xVal == 0 || yVal == 0) {
-      return 0;
+        if(xVal == 0 || yVal == 0)
+        {
+            return 0;
+        }
+
+        return xVal.CompareTo(yVal);
     }
 
-    return xVal.CompareTo(yVal);
-  }
+    public byte ConvertVal(string name)
+    {
+        string prefix = name.Substring(0, name.IndexOf("_"));
 
-  public byte ConvertVal(string name) {
-    string prefix = name.Substring(0, name.IndexOf("_"));
+        byte val = 0;
+        bool convertable = byte.TryParse(prefix, out val);
 
-    byte val = 0;
-    bool convertable = byte.TryParse(prefix, out val);
+        if(convertable == true)
+        {
+            return val;
+        }
+        else
+        {
+            switch(prefix)
+            {
+                case "A":
+                    return 1;
 
-    if (convertable == true) {
-      return val;
-    } else {
-      switch (prefix) {
-        case "A":
-          return 1;
+                case "J":
+                case "Q":
+                case "K":
+                    return 11;
 
-        case "J":
-        case "Q":
-        case "K":
-          return 11;
+                case "Back":
+                    return 12;
 
-        case "Back":
-          return 12;
+                case "Joker":
+                    return 13;
 
-        case "Joker":
-          return 13;
-
-        default:
-          return 0;
-      }
+                default:
+                    return 0;
+            }
+        }
     }
-  }
 }
